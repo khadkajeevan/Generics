0a96961e7858929c86d9dfd583ad0c754450b5d4
Jeevan Khadka <jeevan.khadka1@outlook.com>
2019-09-28 15:57:13 -0400

    Final commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Generic
 {
@@ -6,7 +9,93 @@ namespace Generic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> list = new List<int>();
+
+            foreach (var x in Enumerable.Range(1, 5))
+            {
+                list.Add(x);
+            }
+
+            Console.WriteLine("List:");
+
+            foreach (var x in list)
+            {
+                Console.WriteLine(x);
+            }
+
+            Queue<int> queue = new Queue<int>();
+
+            foreach (var x in Enumerable.Range(1, 5))
+            {
+                queue.Enqueue(x);
+            }
+
+            Console.WriteLine("Queue:");
+
+            foreach (var x in queue)
+            {
+                Console.WriteLine(x);
+            }
+
+            Stack<int> stack = new Stack<int>();
+
+            foreach (var x in Enumerable.Range(1, 5))
+            {
+                stack.Push(x);
+            }
+
+            Console.WriteLine("Stack:");
+
+            foreach (var x in stack)
+            {
+                Console.WriteLine(x);
+            }
+
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+
+            string a = "A";
+
+            for (int i = 0; i < 5; i++)
+            {
+                dictionary.Add(i, a);
+                a += "A";
+            }
+
+            Console.WriteLine("Dicitonary:");
+
+            foreach (var x in dictionary)
+            {
+                Console.WriteLine(x);
+            }
+
+            SortedList<int, int> sortedList = new SortedList<int, int>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                sortedList.Add(i, (i * 2));
+            }
+
+            Console.WriteLine("Sorted List:");
+
+            foreach (var x in sortedList)
+            {
+                Console.WriteLine(x);
+            }
+
+            HashSet<int> hashSet = new HashSet<int>();
+
+            foreach (var x in Enumerable.Range(1, 5))
+            {
+                hashSet.Add(x);
+            }
+
+            Console.WriteLine("Hash Set:");
+
+            foreach (var x in hashSet)
+            {
+                Console.WriteLine(x);
+            }
         }
     }
 }
+
